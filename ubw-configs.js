c0a98fed4256b17634f84b39b6589e53cccdf78f
kjirou <sorenariblog@gmail.com>
2019-03-03 11:51:18 +0900

    ubw init .

---
+++ ubw-configs.js
@@ -0,0 +1,12 @@
+module.exports = function ubwConfigs() {
+  return {
+    "blogName": "My Blog",
+    "blogUrl": "https://example.com",
+    "publicationDir": "./blog-publication",
+    "cssUrls": [
+      "/external-resources/index.css"
+    ],
+    "language": "en",
+    "timeZone": "UTC"
+  };
+}
