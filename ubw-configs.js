f2250fc90462191d5aaab4dfe68b4b47042b8e79
kjirou <sorenariblog@gmail.com>
2019-03-18 00:21:12 +0900

    Set default og:image / Set a link of kjirou.github.io

--- ubw-configs.js
+++ ubw-configs.js
@@ -1,12 +1,25 @@
+const {createDefaultUbwConfigs} = require('unlimited-blog-works');
+
+const defautConfigs = createDefaultUbwConfigs();
+
 module.exports = function ubwConfigs() {
+  const blogUrl = 'https://kjirou.github.io/blog';
+
   return {
     "blogName": "それなりブログ",
-    "blogUrl": "https://kjirou.github.io/blog",
+    blogUrl,
     "publicationDir": "./docs",
     "cssUrls": [
       "/blog/external-resources/index.css"
     ],
     "language": "ja",
-    "timeZone": "Asia/Tokyo"
+    "timeZone": "Asia/Tokyo",
+    defaultOgpImageUrl: blogUrl + '/external-resources/hagure_metal-300x300.png',
+    additionalTopPageLinks: defautConfigs.additionalTopPageLinks.concat([
+      {
+        linkText: 'https://kjirou.github.io/',
+        href: 'https://kjirou.github.io/',
+      },
+    ]),
   };
 }
